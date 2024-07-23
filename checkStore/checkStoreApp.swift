ef53cc32f25175a94a4b45591e81be5bca48ef41
Axl Estevez <axlestevez@gmail.com>
2024-07-22 21:55:40 -0600

    Initial Commit

---
+++ checkStore/checkStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkStoreApp.swift
+//  checkStore
+//
+//  Created by Axl Estevez on 22/07/24.
+//
+
+import SwiftUI
+
+@main
+struct checkStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
